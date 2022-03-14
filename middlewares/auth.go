1fc73d721f25f88cc30790d29e593d24626a103a
JQ-CHENN <63723461+JQ-CHENN@users.noreply.github.com>
2022-03-14 21:27:52 +0800

    Update auth.go

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -12,8 +12,6 @@ import (
 func JWTAuthMiddleware() func(c *gin.Context) {
 	return func(c *gin.Context) {
 		// 客户端携带Token有三种方式 1.放在请求头 2.放在请求体 3.放在URI
-		// 这里假设Token放在Header的Authorization中，并使用Bearer开头
-		// 这里的具体实现方式要依据你的实际业务情况决定
 		authHeader := c.Request.Header.Get("Authorization")
 		if authHeader == "" {
 			controller.ResponseError(c, controller.CodeNeedLogin)
